4f6ef9556eaa515d813ce203381cd5be0ba85944
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-11-20 09:06:28 -0800

    runPhoneBook switch statement quit

--- Assignment4/src/Assignment4.cpp
+++ Assignment4/src/Assignment4.cpp
@@ -10,11 +10,12 @@
 using namespace std;
 
 void printIntro();
+void runPhoneBook();
 
 int main()
 {
 	printIntro();
-
+	runPhoneBook();
 
 	return 0;
 }
@@ -23,4 +24,49 @@ inline void printIntro()
 {
 	cout << "***MY	PHONEBOOK	APPLICATION***" << endl;
 	cout << "Please	choose	an	operation:" << endl;
+}
+
+inline void runPhoneBook()
+{
+	bool running = true;
+
+	while (running)
+	{
+		string input;
+		cout
+		        << "A(Add) | L(Load) | S(Search) | P(Print) | F(Filter) | Q(Quit): ";
+		cin >> input;
+
+		if(input == "A")
+		{
+
+		}
+		else if(input == "L")
+		{
+
+		}
+		else if(input == "S")
+		{
+
+		}
+		else if(input == "P")
+		{
+
+		}
+		else if(input == "F")
+		{
+
+		}
+		else if(input == "Q")
+		{
+			cout << "Bye" << endl;
+			running = false;
+		}
+		else
+		{
+
+		}
+	}
+
+
 }
